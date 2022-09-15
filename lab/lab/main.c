ff83d8723e5dd0487f2fa9344b2a2c363561e198
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-09-15 20:03:29 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
-#define PI 3.1415
- 
-int main()
-{
-    float r, h;
-    double area = 0;
-    double poln = 0;
-    double volume = 0;
+
+int main(){
+    int i,j;
+    i = 10;
+    j = ++i;
+    printf("%d\n", j);
+    i = 10;
+    j = i++;
+    printf("%d\n", j);
     
-    printf("Введите радиус цилиндра: \n");
-    scanf("%f", &r);
-    
-    printf("Введите высоту цилиндра: \n");
-    scanf("%f", &h);
- 
-    area = 2 * r * h * PI;
-    printf("Площадь боковой поверхности цилиндра: %.2f\n", area);
-    
-    poln = 2 * PI * r *(r + h);
-    printf("Площадь полной поверхности цилиндра: %.2f\n", poln);
-    
-    volume = PI * r * r * h;
-    printf("Объем цилиндра: %.2f\n", volume);
-    
-    return 0;
 }
--- lab_2_3.c
+++ lab_2_3.c
@@ -1,7 +1,8 @@
 //Л.Р. 2 задание 3 задача 15
 #include <stdio.h>
-#include <math.h>
+//#include <math.h>
 
+const float PI = 3.1415926535897932384626;
 int main()
 {
 
@@ -11,12 +12,19 @@ int main()
     printf("Введите значение в градусах: \n");
     scanf("%d", &degree);
     
-    x = degree*M_PI/180;
+    x = degree*PI/180;
     
     term = x;
     sine = term;
     
-    while(fabs(term)>0.00001)
+    if (term <0){
+        term = -term;
+        }
+    else {
+        term = term;
+        
+    }
+    while(term>0.00001)
     {
         i=i+2;
         term=-term*x*x/(i*(i-1));
