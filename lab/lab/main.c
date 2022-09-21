214aeb812505e02a17d401f83e78be913b89d305
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-09-21 12:28:42 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-
-int main (){
-    printf("Hello");
+int main()
+{
+  int a[5]; // объявлен массив a из 5 элементов
+  int i;
+  // Ввод элементов массива
+  for (i = 0; i<5; i++)
+  {
+    printf("a[%d] = ", i);
+    scanf("%d", &a[i]); // &a[i] - адрес i-го элемента массива
+  }
+  // Вывод элементов массива
+  for (i = 0; i<5; i++)
+    printf("%d ", a[i]); // пробел в формате печати обязателен
+  getchar(); getchar();
+  return 0;
 }
