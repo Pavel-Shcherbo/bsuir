63c33004605fff41b815edbc05290fb49bc402cb
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-09-28 01:34:35 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -1,14 +1,42 @@
+//Л.Р. 3 задание 2 задача 14
 //Л.Р. 3 задание 3 задача 14
 #include <stdio.h>
 
- 
 int main()
 {
-    int n, i = 0;
-    scanf("%i", &n);
-
-    int mass[100];
-    for (int i = 0; i < n; i++) {
-        scanf("%i", &mass[i]);}
-    printf("%d", mass[i]);
+    int a[100],i,n,min,max, idx_min = 0;
+  
+   printf("Enter size of the array : ");
+   scanf("%d",&n);
+    while (n > 100 || n < 2) {
+        printf("Pls try again\n");
+        rewind(stdin);
+        printf("Enter size of the array : ");
+        scanf("%d",&n);
+    }
+    
+   printf("Enter elements in array : ");
+   for(i=0; i<n; i++)
+   {
+       scanf("%d",&a[i]);
+   }
+    printf("Array: \n");
+    for(int i=0; i<n; i++) {
+         printf("%d ", a[i]);
+      }
+   min=max=a[0];
+   for(i=1; i<n; i++)
+   {
+        if(min>a[i])
+         min=a[i];
+          if(max<a[i])
+           max=a[i];
+   }
+    printf("minimum of array is : %d",min);
+   
+    for (int i = 0; i < n; ++i) if (a[i] == min) idx_min = i;
+     
+       printf("\n%d\n", idx_min);
+    
+   return 0;
 }
--- lab_3_2.c
+++ lab_3_2.c
@@ -1,9 +1,48 @@
 //Л.Р. 3 задание 2 задача 14
+//Л.Р. 3 задание 3 задача 14
 #include <stdio.h>
 
- 
 int main()
 {
+    int a[100],i,n,min,max, idx_min = 0, idx_max = 0;
+  
+   printf("Enter size of the array : ");
+   scanf("%d",&n);
+    while (n > 100 || n < 2) {
+        printf("Pls try again\n");
+        rewind(stdin);
+        printf("Enter size of the array : ");
+        scanf("%d",&n);
+    }
     
-    return 0;
+   printf("Enter elements in array : ");
+   for(i=0; i<n; i++)
+   {
+       scanf("%d",&a[i]);
+   }
+    printf("Array: \n");
+    for(int i=0; i<n; i++) {
+         printf("%d ", a[i]);
+      }
+   min=max=a[0];
+   for(i=1; i<n; i++)
+   {
+        if(min>a[i])
+         min=a[i];
+          if(max<a[i])
+           max=a[i];
+   }
+    printf("minimum of array is : %d",min);
+    printf("\nmaximum of array is : %d\n",max);
+   
+    printf("Array: \n");
+    for(int i=0; i<n; i++) {
+         printf("%d ", a[i]);
+      }
+    for (int i = 0; i < n; ++i) if (a[i] == max) idx_max = i;
+    for (int i = 0; i < n; ++i) if (a[i] == min) idx_min = i;
+     
+       printf("\n%d\n", idx_max);
+    
+   return 0;
 }
