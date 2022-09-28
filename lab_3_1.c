eaf68ff2e6da89799a0c21cfe37fddd83801acb1
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-09-29 00:22:54 +0300

    1

--- lab_3_1.c
+++ lab_3_1.c
@@ -1,9 +1,61 @@
 //Л.Р. 3 задание 1 задача 14
 #include <stdio.h>
 
- 
 int main()
 {
+    int a[100], i, n, min, idx_min = 0, sum = 0;
+  
+    printf("Enter size of the array : ");
+    scanf("%d", &n);
+
+    while (n > 100 || n < 2 || n % 1 != 0) {
+        printf("Pls try again\n");
+        rewind(stdin);
+        printf("Enter size of the array : ");
+        scanf("%d", &n);
+    }
     
-    return 0;
+    printf("Enter elements in array : ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Array: \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    min = a[0];
+
+    for (i = 1; i < n; i++)
+    {
+        if (min > a[i])
+        {
+            min = a[i];
+            idx_min = i;
+        }
+    }
+    printf("\nminimum of array is : %d",min);
+    printf("\n Position of min el: %d\n", idx_min);
+
+
+
+    int flag = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (flag && a[i] < 0)
+        {
+            flag = 0;
+            break;
+        }
+        else if (a[i] < 0) flag = 1;
+        else if (flag)sum = sum + a[i];
+    }
+
+    if (!flag)printf("\nSumma el: %d\n", sum);
+    else printf("NETY 2 OTRIC");
+  
+   return 0;
 }
--- lab_3_2.c
+++ lab_3_2.c
@@ -1,29 +1,43 @@
 //Л.Р. 3 задание 2 задача 14
-//Л.Р. 3 задание 3 задача 14
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
+void arr_rand_input(int a[], int n);
 
 int main()
 {
-    int a[100],i,n,min,max, idx_min = 0, idx_max = 0;
-  
+    int a[100],i,n,min,max, idx_min = 0, idx_max = 0,choice=0;
    printf("Enter size of the array : ");
    scanf("%d",&n);
+    printf("Print 1 if you want to enter the elements manually or print 2 to fill the arry with  random numbers\n");
+    scanf("%i",&choice);
+    switch (choice)
+    {
+        case 1:
+            for(i=0; i<n; i++)
+            {
+                scanf("%d",&a[i]);
+            }
+            printf("Array: \n");
+            for(int i=0; i<n; i++) {
+                printf("%d ", a[i]);
+            }
+            break;
+        case 2:
+            arr_rand_input(a,n);
+            break;
+        default:
+            return 0;
+            break;
+    }
     while (n > 100 || n < 2) {
         printf("Pls try again\n");
         rewind(stdin);
         printf("Enter size of the array : ");
         scanf("%d",&n);
     }
-    
-   printf("Enter elements in array : ");
-   for(i=0; i<n; i++)
-   {
-       scanf("%d",&a[i]);
-   }
-    printf("Array: \n");
-    for(int i=0; i<n; i++) {
-         printf("%d ", a[i]);
-      }
    min=max=a[0];
    for(i=1; i<n; i++)
    {
@@ -34,6 +48,7 @@ int main()
    }
     printf("minimum of array is : %d",min);
     printf("\nmaximum of array is : %d\n",max);
+    
    
     printf("Array: \n");
     for(int i=0; i<n; i++) {
@@ -43,6 +58,36 @@ int main()
     for (int i = 0; i < n; ++i) if (a[i] == min) idx_min = i;
      
        printf("\n%d\n", idx_max);
+    if(idx_min < idx_max)
+    {
+        for(int i = idx_max;i>idx_min+1; i-- )
+        {
+            a[i -1] = a[i];
+            n--;
+        }
+    }else if (idx_min>idx_max)
+    {
+        if(idx_min > idx_max)
+        {
+            for(int i = idx_min;i>idx_max+1; i-- )
+            {
+                a[i -1] = a[i];
+                n--;
+            }
+        }
+    }
+    printf("Array: \n");
+    for(int i=0; i<n; i++) {
+         printf("%d ", a[i]);
+      }
     
    return 0;
+}
+
+void arr_rand_input(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand();
+    }
 }
