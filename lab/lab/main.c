500b2e24d41c5bc4fc33a5fe3335dc78e3c96ac9
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-09-29 09:24:23 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -1,18 +1,20 @@
-//Л.Р. 3 задание 1 задача 14
+//Л.Р. 3 задание 3 задача 14
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 int main()
 {
-    int a[100], b[100], i, n, flag = 0, p;
+    int a[100], b[100], i, n=0, flag = 0, p;
     bool boolean = 0;
-
-    printf("Enter size of the array : ");
-    scanf("%d", &n);
-
-    printf("choose 1to random and 2for kb input\n");
-     scanf("%i", &p);
+    
+    while (n > 100 || n < 2 || n % 1 != 0) {
+        rewind(stdin);
+        printf("Enter size of the array : ");
+        scanf("%d", &n);
+    }
+    printf("Choose 1 to random and 2 for manual input\n");
+    scanf("%i", &p);
     switch (p) {
         case 1:
             for (int i = 0; i < n; i++) {
@@ -22,87 +24,57 @@ int main()
                 b[i] = (rand() %10);
                 printf("the %dth element of array is %d\n", i, b[i]);}
             break;
-            
         case 2:
-            while (n > 100 || n < 2 || n % 1 != 0) {
-                printf("Pls try again\n");
-                rewind(stdin);
-                printf("Enter size of the array : ");
-                scanf("%d", &n);
-            }
-            
             printf("Enter elements in array_1 : ");
-            for (i = 0; i < n; i++)
-            {
-                
+            for (i = 0; i < n; i++){
                 boolean = scanf("%d", &a[i]);
                 while (boolean != 1) {
                     printf("Pls try again\n");
                     rewind(stdin);
-                    boolean = scanf("%d", &a[i]);
-                }
+                    boolean = scanf("%d", &a[i]);}
             }
-            
             printf("Enter elements in array_2 : ");
-            for (i = 0; i < n; i++)
-            {
-                
+            for (i = 0; i < n; i++){
                 boolean = scanf("%d", &b[i]);
                 while (boolean != 1) {
                     printf("Pls try again\n");
                     rewind(stdin);
-                    boolean = scanf("%d", &b[i]);
-                }
+                    boolean = scanf("%d", &b[i]);}
             }
-            break;
-    }
-
+        break;}
+    
     printf("\nArray_1: \n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-
+    for (int i = 0; i < n; i++){
+        printf("%d ", a[i]);}
     printf("\nArray_2: \n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", b[i]);
-    }
+    for (int i = 0; i < n; i++){
+        printf("%d ", b[i]);}
     i = 0;
     while (flag != 1) {
         if (a[i] != b[i]) {
             break;
         }
-        i++;
-    }
+        i++;}
     if (a[i] > b[i]) {
         for (i = n - 1; i >= 0; i--) {
             a[i] = a[i] - b[i];
             if (a[i] < 0) {
                 a[i] += 10;
-                a[i - 1] -= 1;
-            }
+                a[i - 1] -= 1;}
         }
         printf("\nArray_itog: \n");
-        for (int i = 0; i < n; i++)
-        {
-            printf("%d ", a[i]);
-        }
+        for (int i = 0; i < n; i++){
+            printf("%d ", a[i]);}
     }
     else {
         for (i = n - 1; i >= 0; i--) {
             b[i] = b[i] - a[i];
             if (b[i] < 0) {
                 b[i] += 10;
-                b[i - 1] -= 1;
-            }
+                b[i - 1] -= 1;}
         }
         printf("\nArray_itog: \n");
-        for (int i = 0; i < n; i++)
-        {
-            printf("%d ", b[i]);
-        }
+        for (int i = 0; i < n; i++){
+            printf("%d ", b[i]);}
     }
-
-    return 0;
-}
+    return 0;}
--- lab_3_2.c
+++ lab_3_2.c
@@ -1,63 +1,50 @@
 //Л.Р. 3 задание 2 задача 14
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-
-
-void arr_rand_input(int a[], int n);
 
 int main()
 {
-    int a[100],i,n,min,max, idx_min = 0, idx_max = 0,choice=0;
-   printf("Enter size of the array : ");
-   scanf("%d",&n);
-    printf("Print 1 if you want to enter the elements manually or print 2 to fill the arry with  random numbers\n");
-    scanf("%i",&choice);
-    switch (choice)
-    {
-        case 1:
-            for(i=0; i<n; i++)
-            {
-                scanf("%d",&a[i]);
-            }
-            printf("Array: \n");
-            for(int i=0; i<n; i++) {
-                printf("%d ", a[i]);
-            }
-            break;
-        case 2:
-            arr_rand_input(a,n);
-            break;
-        default:
-            return 0;
-            break;
-    }
+    int a[100],i,n = 0,min,max, idx_min = 0, idx_max = 0, p;
+    bool boolean = 0;
+
     while (n > 100 || n < 2) {
-        printf("Pls try again\n");
         rewind(stdin);
         printf("Enter size of the array : ");
-        scanf("%d",&n);
-    }
-   min=max=a[0];
-   for(i=1; i<n; i++)
-   {
-        if(min>a[i])
-         min=a[i];
-          if(max<a[i])
-           max=a[i];
-   }
-    printf("minimum of array is : %d",min);
-    printf("\nmaximum of array is : %d\n",max);
+        scanf("%d",&n);}
+    printf("Choose 1 to random and 2 for manual input\n");
+    scanf("%i", &p);
+    switch (p) {
+        case 1:
+            for (int i = 0; i < n; i++) {
+                a[i] = (rand() % 10 );
+                printf("the %dth element of array is %d\n", i, a[i]);}
+            break;
+        case 2:
+            printf("Enter elements in array : ");
+            for (i = 0; i < n; i++){
+                boolean = scanf("%d", &a[i]);
+                while (boolean != 1) {
+                    printf("Pls try again\n");
+                    rewind(stdin);
+                    boolean = scanf("%d", &a[i]);}}
+        break;}
     
-   
+    min=max=a[0];
+    for(i=1; i<n; i++){
+        if(min>a[i])
+            min=a[i];
+        if(max<a[i])
+            max=a[i];}
+    
     printf("Array: \n");
     for(int i=0; i<n; i++) {
-         printf("%d ", a[i]);
-      }
+        printf("%d ", a[i]);}
+    
     for (int i = 0; i < n; ++i) if (a[i] == max) idx_max = i;
     for (int i = 0; i < n; ++i) if (a[i] == min) idx_min = i;
-     
-       printf("\n%d\n", idx_max);
+    
     if(idx_min < idx_max)
     {
         for(int i = idx_max;i>idx_min+1; i-- )
@@ -65,7 +52,8 @@ int main()
             a[i -1] = a[i];
             n--;
         }
-    }else if (idx_min>idx_max)
+    }
+    else if (idx_min>idx_max)
     {
         if(idx_min > idx_max)
         {
@@ -76,18 +64,12 @@ int main()
             }
         }
     }
-    printf("Array: \n");
+    printf("\nArray_new: \n");
     for(int i=0; i<n; i++) {
-         printf("%d ", a[i]);
-      }
+        printf("%d ", a[i]);
+    }
+    return 0;
     
-   return 0;
 }
 
-void arr_rand_input(int a[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        a[i] = rand();
-    }
-}
+
