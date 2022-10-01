a53088f2cf95fc1dd50d88dbb01c4819717da9a7
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-10-01 23:43:56 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -73,7 +73,7 @@ int main()
                 b[i] += 10;
                 b[i - 1] -= 1;}
         }
-        printf("\nArray_itog: \n");
+        printf("\nArray_itog: \n-");
         for (int i = 0; i < n; i++){
             printf("%d ", b[i]);}
     }
