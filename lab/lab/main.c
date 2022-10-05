55e89b81cc1e6783eebb96b94ad3e9c0b408396d
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-10-05 22:53:14 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -75,6 +75,6 @@ int main()
         }
         printf("\nArray_itog: \n");
         for (int i = 0; i < n; i++){
-            printf("%d ", b[i]);}
+            printf("%d saf ", b[i]);}
     }
     return 0;}
