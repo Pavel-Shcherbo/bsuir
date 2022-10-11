eccd29d95a294fc84fb01b51f3c9433436ef4f64
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-10-11 03:21:18 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -1,41 +1,68 @@
-//Л.Р. 4 задание 1 задача 14
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
+#include <time.h>
 int main() {
-    int n, m, p;
-    int a[100][100];
-    printf("enter the size of matrix NxM\n");
+    int n, p;
+    int a[100][100], sum=0;
+    bool negative = false;
+
     printf("enter the value of the double array (strok)\n");
     scanf("%i", &n);
-    printf("enter the value of the double array (stolb)\n");
-    scanf("%i", &m);
-    printf("choose 1to random and 2for kb input\n");
+    printf("1==input, 2==random\n");
     scanf("%i", &p);
+    
+    srand(time(0));
     switch (p) {
-    case 1:
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < n; k++)
-            {
-                printf("arr[%d][%d]=", j, k);
-                scanf("%d", &a[j][k]);
+        case 1:
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++)
+                {
+                    printf("arr[%d][%d]=", j, k);
+                    scanf("%d", &a[j][k]);
+                }
+                
             }
-
-        }
-        break;
-    case 2:
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < n; k++)
-            {
-                printf("arr[%d][%d]=", j, k);
-                a[j][k] = (rand() % 100-40);
-                printf("%d\n", a[j][k]);
+            break;
+        case 2:
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++)
+                {
+                    
+                    printf("arr[%d][%d]=", j, k);
+                    a[j][k] = (rand() % 10-3);
+                    printf("%d\n", a[j][k]);
+                }
             }
-        }
-        break;
-    default:
-        printf("stupid");
+            break;
+        default:
+            printf("stupid");
     }
     
-    return 0;}
+    for(int j=0;j<n;j++){
+        for(int k=0;k<n;k++){
+            printf("%d ",a[j][k]);
+        }
+        printf("\n");
+    }
+      for(unsigned int j = 0; j < n; j++)
+      { sum = 0;
+         for(unsigned int k=0;k<n;k++)
+         {
+             sum =sum + a[j][k];
+            if(a[j][k] < 0)
+               negative = true;
+         }
+         if(!negative)
+             printf("\nSum of %d row: %d\t", (j+1), sum);
+
+         else
+         {
+            negative = false;
+             printf("\nLine %d has a negative element\t",(j+1));
+
+           
+         }
+      }
+        return 0;
+    }
