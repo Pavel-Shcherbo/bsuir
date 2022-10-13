885bdec0b8ae429ccf0ec7bab3ee340c544ac016
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-10-14 00:10:26 +0300

    1

--- lab/lab/main.c
+++ lab/lab/main.c
@@ -1,20 +1,14 @@
-//Л.Р. 4 задание 2 задача 14
+//Л.Р. 4 задание 3 задача 14
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <time.h>
 int main() {
-    int n=0, m=0, p=0;
-    int a[100][100], sum=0;
-    bool negative = false;
+    int n=0, p=0, min=100;
+    int a[100][100];
     while (n > 100 || n < 2) {
         rewind(stdin);
-        printf("enter the number of rows\n");
+        printf("Enter size of the array : ");
         scanf("%d",&n);}
-    while (m > 100 || m < 2) {
-        rewind(stdin);
-        printf("enter the number of columns\n");
-        scanf("%d",&m);}
     while (p > 2 || p < 1) {
         rewind(stdin);
         printf("Press 1 to manual input, press 2 to random\n");
@@ -23,7 +17,7 @@ int main() {
     switch (p) {
         case 1:
             for (int j = 0; j < n; j++) {
-                for (int k = 0; k < m; k++)
+                for (int k = 0; k < n; k++)
                 {
                     printf("arr[%d][%d]=", j, k);
                     scanf("%d", &a[j][k]);
@@ -32,9 +26,9 @@ int main() {
             break;
         case 2:
             for (int j = 0; j < n; j++) {
-                for (int k = 0; k < m; k++)
+                for (int k = 0; k < n; k++)
                 {
-                    a[j][k] = (rand() % 21-10);
+                    a[j][k] = (rand() % 20-9);
                 }
             }
             break;
@@ -42,40 +36,26 @@ int main() {
             printf("stupid");
     }
     for(int j=0;j<n;j++){
-        for(int k=0;k<m;k++){
+        for(int k=0;k<n;k++){
             printf("%d\t",a[j][k]);
         }
         printf("\n");
     }
-    for(unsigned int j = 0; j < n; j++)
-    {
-        negative = true;
-        sum = 0;
-        for(unsigned int k=0;k<n;k++)
-        {
-            if(a[k][j] >= 0) {
-                negative = false;
-                break;
-            }
+    printf("\n");
+    for(int j=0;j<n/2;j++){
+        for(int k=n/2;k<n;k++){
+            printf("%d\t",a[j][k]);
         }
-        if(negative)
-        {
-            for(unsigned int k=0;k<n;k++)
-                sum =sum + a[k][j];
-                sum = sum / n;
-            for (int j = 0; j < n; j++)
-                for (int k = 0; k < m; k++)
-                    a[j][k] -= sum;
-            printf("\n\n");
-            for(int j=0;j<n;j++){
-                for(int k=0;k<m;k++){
-                    printf("%d\t",a[j][k]);
-                }
-                printf("\n");
-            }
-            break;
-        }
-        else if (j == n - 1)
-            printf("There is no negatives columns\n");
+        printf("\n");
     }
+    printf("\n");
+    for(int j=0;j<n/2;++j){
+        for(int k=n/2;k<n;++k){
+            if (a[j][k] < min)
+            {
+                min = a[j][k];
+            }
+        }
+    }
+    printf("min = %d\n", min);
     return 0;}
