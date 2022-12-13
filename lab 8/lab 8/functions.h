30a97c0017c40d1fb65b69c4a74f2345f9f86d88
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-12-13 09:26:20 +0300

    1

--- lab 8/lab 8/functions.h
+++ lab 8/lab 8/functions.h
@@ -1,1 +1,9 @@
+#pragma once
 
+void *input_str(char** string);
+void *str_len(char* str, int* len);
+void *choose(int n);
+void *merge_sort(int* mas_i, int* mas, int l, int r);
+void merge(int* mas_i, int* mas, int left, int right);
+void *longest_words(char** str, int* length, int n, int* max);
+void *sort_string(int argc, char** argv);
